676d0bc18ddd742514f36f25db390903c1c60656
Deepanshu Kumar <86040514+anshuk6469@users.noreply.github.com>
2024-09-26 13:13:21 +0530

    Update rg.tf

--- rg.tf
+++ rg.tf
@@ -2,3 +2,8 @@ resource "azurerm_resource_group" "example" {
    name = "deep-rg1"
    location = "eastus"
 }
+
+resource "azurerm_resource_group" "example5" {
+   name = "deep-rg5"
+   location = "eastus"
+}
